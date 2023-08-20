1934b71c1934de4dfc374395c3d03d0444bd7fcd
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-08-20 20:39:55 +0300

    Configure consumer

--- src/main/java/com/yllu/kafkajava/Consumer.java
+++ src/main/java/com/yllu/kafkajava/Consumer.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -21,20 +22,39 @@ public class Consumer {
 
     public Consumer() {
         kafkaConsumer = new KafkaConsumer<>(KafkaConfigs.setupProperties());
+        shutDownConsumerOnExit();
     }
 
 
     public void poll(String[] topics) {
 
-        kafkaConsumer.subscribe(Arrays.asList(topics));
+        try {
+            kafkaConsumer.subscribe(Arrays.asList(topics));
+            while (true) {
+                log.info("Polling");
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
 
-        while (true) {
-            log.info("Polling");
-            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
-
-            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
-                LogNameParameters.logKafkaConsumerRecord(consumerRecord);
+                for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                    LogNameParameters.logKafkaConsumerRecord(consumerRecord);
+                }
             }
+        } catch (WakeupException wakeupException) {
+            log.error("Shutting down the consumer due to wakeup exception", wakeupException);
+        } finally {
+            kafkaConsumer.close();
+            log.info("The consumer is gracefully shut down");
         }
+    }
+
+    private void shutDownConsumerOnExit() {
+        final Thread currentThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            kafkaConsumer.wakeup();
+            try {
+                currentThread.join();
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+        }));
     }
 }
